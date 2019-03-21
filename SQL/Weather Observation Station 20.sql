02df87a3575962f91f01d86e6539718b307be887
Adam Taylor-Reece <44418082+Ardraaken@users.noreply.github.com>
2019-03-21 23:32:29 +0000

    Create Weather Observation Station 20.sql

---
+++ SQL/Weather Observation Station 20.sql
@@ -0,0 +1,8 @@
+SELECT CAST(LAT_N AS DECIMAL (7,4))
+FROM
+    (SELECT LAT_N, ROW_NUMBER() OVER (ORDER BY LAT_N) as ROWN
+     FROM STATION 
+     ) AS X
+WHERE ROWN = ( SELECT ROUND((COUNT(LAT_N)+1)/2,0) 
+                FROM STATION
+               );
